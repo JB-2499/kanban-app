36ad7de8fdbe9141798ef0e9fd733daa31c57246
João Barreto <anothermailb@gmail.com>
2026-09-12 16:11:26 -0300

    Implementar camada Service.

---
+++ src/main/java/barreto/kanbanApp/service/TaskService.java
@@ -0,0 +1,49 @@
+package barreto.kanbanApp.service;
+
+import barreto.kanbanApp.exception.ResourceNotFoundException;
+import barreto.kanbanApp.model.Task;
+import barreto.kanbanApp.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task findById(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task com id " + id + " não encontrada."));
+    }
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    public Task save(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task update(Long id, Task task) {
+        Task oldTask = findById(id);
+
+        oldTask.setStatus(task.getStatus());
+        oldTask.setPosition(task.getPosition());
+        oldTask.setTitle(task.getTitle());
+        oldTask.setDescription(task.getDescription());
+        oldTask.setUpdatedAt(LocalDateTime.now());
+
+        return oldTask;
+    }
+
+    public Task deleteById(Long id) {
+        Task task = findById(id);
+        taskRepository.delete(task);
+        return task;
+    }
+}
