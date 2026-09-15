34bb25c5fa61428d9e515829b369ef0187a13afc
João Barreto <anothermailb@gmail.com>
2026-09-14 21:36:27 -0300

    Implementar a camada de service

---
+++ kanban-frontend/src/app/services/task.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task, TaskRequest, TaskStatus, MoveTaskRequest} from '../models/task.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TaskService {
+    private apiUrl = 'http://localhost:8080/tasks';
+
+    constructor(private http: HttpClient) {}
+
+    findAll() {
+        return this.http.get(this.apiUrl);
+    }
+
+    create(dto: TaskRequest) {
+        return this.http.post(this.apiUrl, dto);
+    }
+
+    update(id: number, dto: TaskRequest) {
+        return this.http.put(`${this.apiUrl}/${id}`, dto);
+    }
+
+    delete(id: number) {
+        return this.http.delete(`${this.apiUrl}/${id}`);
+    }
+
+    move(id: number, dto: MoveTaskRequest) {
+        return this.http.patch(`${this.apiUrl}/${id}`, dto);
+    }
+}
