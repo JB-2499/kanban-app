95234767db1e3339a57364d6f4cf4178e83f0139
João Barreto <anothermailb@gmail.com>
2026-09-14 21:14:07 -0300

    Implementar model para o front

---
+++ kanban-frontend/src/app/models/task.model.ts
@@ -0,0 +1,15 @@
+export enum TaskStatus {
+  TO_DO = 'TO_DO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE'
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  status: TaskStatus;
+  position: number;
+  createdAt: string;
+  updatedAt: string;
+}
