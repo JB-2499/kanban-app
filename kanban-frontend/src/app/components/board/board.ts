b3e0b45c3989169fc9d1100c5c17a93e0dca28e3
João Barreto <anothermailb@gmail.com>
2026-09-18 17:46:26 -0300

    Implementar Board.

--- kanban-frontend/src/app/components/board/board.ts
+++ kanban-frontend/src/app/components/board/board.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task, TaskStatus } from '../../models/task.model';
+import { Column } from '../column/column';
 
 @Component({
-  imports: [],
+  imports: [Column],
   selector: 'app-board',
   styleUrl: './board.css',
   templateUrl: './board.html',
 })
-export class Board {}
+export class Board implements OnInit {
+  private taskService = inject(TaskService);
+
+  todoTasks: Task[] = [];
+  inProgressTasks: Task[] = [];
+  doneTasks: Task[] = [];
+
+  ngOnInit() {
+    this.taskService.findAll().subscribe((data: any) => {
+      this.todoTasks = data['TO_DO'] ?? [];
+      this.inProgressTasks = data['IN_PROGRESS'] ?? [];
+      this.doneTasks = data['DONE'] ?? [];
+    });
+  }
+} 
