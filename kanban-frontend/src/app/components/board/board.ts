db4e525bdff7d8f7272683aea889bf15843dd452
João Barreto <anothermailb@gmail.com>
2026-09-18 18:03:01 -0300

    Conectar back e front.

--- kanban-frontend/src/app/components/board/board.ts
+++ kanban-frontend/src/app/components/board/board.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { Task, TaskStatus } from '../../models/task.model';
 import { Column } from '../column/column';
@@ -11,16 +11,20 @@ import { Column } from '../column/column';
 })
 export class Board implements OnInit {
   private taskService = inject(TaskService);
+  private cdr = inject(ChangeDetectorRef);
 
   todoTasks: Task[] = [];
   inProgressTasks: Task[] = [];
   doneTasks: Task[] = [];
 
   ngOnInit() {
+    console.log('ngOnInit chamado');
     this.taskService.findAll().subscribe((data: any) => {
+      console.log('dados recebidos:', data);
       this.todoTasks = data['TO_DO'] ?? [];
       this.inProgressTasks = data['IN_PROGRESS'] ?? [];
       this.doneTasks = data['DONE'] ?? [];
+      this.cdr.detectChanges();
     });
   }
-} 
+}
