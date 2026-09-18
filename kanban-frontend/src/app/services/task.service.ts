083eff0652f015a56db1f0698a28d791c2643117
João Barreto <anothermailb@gmail.com>
2026-09-18 17:45:32 -0300

    Corrigir erro no service.

--- kanban-frontend/src/app/services/task.service.ts
+++ kanban-frontend/src/app/services/task.service.ts
@@ -1,32 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Task, TaskRequest, TaskStatus, MoveTaskRequest} from '../models/task.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class TaskService {
-    private apiUrl = 'http://localhost:8080/tasks';
+  private apiUrl = 'http://localhost:8080/tasks';
+  
+  private http = inject(HttpClient);
 
-    constructor(private http: HttpClient) {}
+  findAll() {
+    return this.http.get(this.apiUrl);
+  }
 
-    findAll() {
-        return this.http.get(this.apiUrl);
-    }
+  create(dto: TaskRequest) {
+    return this.http.post(this.apiUrl, dto);
+  }
 
-    create(dto: TaskRequest) {
-        return this.http.post(this.apiUrl, dto);
-    }
+  update(id: number, dto: TaskRequest) {
+    return this.http.put(`${this.apiUrl}/${id}`, dto);
+  }
 
-    update(id: number, dto: TaskRequest) {
-        return this.http.put(`${this.apiUrl}/${id}`, dto);
-    }
+  delete(id: number) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 
-    delete(id: number) {
-        return this.http.delete(`${this.apiUrl}/${id}`);
-    }
-
-    move(id: number, dto: MoveTaskRequest) {
-        return this.http.patch(`${this.apiUrl}/${id}/move`, dto);
-    }
+  move(id: number, dto: MoveTaskRequest) {
+    return this.http.patch(`${this.apiUrl}/${id}/move`, dto);
+  }
 }
